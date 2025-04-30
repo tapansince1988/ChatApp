06eb8afef10f5790d6a8d2a8c54a117614892e96
tapan <tapansince1988@gmail.com>
2025-04-30 13:39:21 +0530

    reveiver function updated to handle video and script updated to handle to show video file

--- chat/consumers.py
+++ chat/consumers.py
@@ -21,15 +21,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
 
     async def receive(self, text_data):
+
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         username = text_data_json["username"]
-        await self.channel_layer.group_send(
-            self.roomGroupName,{
-                "type" : "sendMessage" ,
-                "message" : message , 
-                "username" : username ,
-            })
+        data = json.loads(text_data)
+        message_type = data.get('type')
+        # message = data.get('message')
+
+        if message_type == 'video':
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'message_type': 'video'
+                }
+            )
+        else:
+            await self.channel_layer.group_send(
+                self.roomGroupName,{
+                    "type" : "sendMessage" ,
+                    "message" : message , 
+                    "username" : username ,
+                })
         
         
     async def sendMessage(self , event) : 
